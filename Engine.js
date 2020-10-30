2aa942ae84a3773fb0fe0754cb0b08f4f66b4374
Packmanager9 <36295363+Packmanager9@users.noreply.github.com>
2020-10-30 02:05:18 -0700

    E = error

--- Engine.js
+++ Engine.js
@@ -571,7 +571,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
 
 
-    function setUp(canvas_pass, style) {E
+    function setUp(canvas_pass, style) {
 
         canvas = canvas_pass
         canvas_context = canvas.getContext('2d');
